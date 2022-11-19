9983c2967a51783e230870ceb0ece95c2d9729fe
Clardene Jones <clardene.jones@gmail.com>
2022-11-19 16:11:39 -0500

    updated local storage

--- assets/js/library.js
+++ assets/js/library.js
@@ -1,120 +1,130 @@
 //Grab all elements with an attribute of key
-let userInput = document.querySelectorAll("[key]") 
-let taskList = []; //Each represented by Task.
+let userInput = document.querySelectorAll("[key]")
+let taskList = [];
 let idCounter = 0;
-let siblings = n => [...n.parentElement.children].filter(c=>c!=n) //Returns all siblings of an element
-const getObjFromForm = () => [...userInput].reduce((a, v) => ({ ...a, [v.getAttribute("key")]: v.value}), {}) //Returns form input as an object
-
+let siblings = n => [...n.parentElement.children].filter(c=>c!=n)
+const getObjFromForm = n => [...userInput].reduce((a, v) => ({ ...a, [v.getAttribute("key")]: v.value}), {})
+ 
 function addItem(arr){
-  //Populate data with user's input if no arguments
-  let data = (arr === undefined) ? getObjFromForm() : arr
-  let id = idCounter
-  idCounter++;
-
-  //Push to taskList
-  taskList.push(new Task(data, id))
-  taskList[id].display(document.getElementsByClassName("list-group")[0])
+ //Populate data with user's input if no arguments
+ let data = (arr === undefined) ? getObjFromForm() : arr
+ let id = idCounter
+ idCounter++;
+ 
+ taskList.push(new Task(data, id))
+ taskList[id].display(document.getElementsByClassName("list-group")[0])
 }
-
+ 
 function validateInput(){
-  //Check for empty fields. Returns false if any input is missing. True is everything looks good.
-  let pass = true
-  
-  //forEach loop. 
-  //equivalent to for(let i in userInput)
-  userInput.forEach((i)=>{
-    let parent = i.parentElement;
-    let val = i.value 
-    if(val === ""){ 
-      parent.querySelector(".alert-warning").classList.add("visible")
-      parent.querySelector(".alert-warning").classList.remove("invisible")
-      pass = false  
-    }else{
-      parent.querySelector(".alert-warning").classList.remove("visible")
-      parent.querySelector(".alert-warning").classList.add("invisible")
-    }
-  })
-  return pass
+ //Check for empty fields.
+ let pass = true
+ userInput.forEach((i, j)=>{
+   let parent = i.parentElement;
+   let val = "";
+   val = i.value
+   if(val === ""){
+     parent.querySelector(".alert-warning").classList.add("visible")
+     parent.querySelector(".alert-warning").classList.remove("invisible")
+     pass = false 
+   }else{
+     parent.querySelector(".alert-warning").classList.remove("visible")
+     parent.querySelector(".alert-warning").classList.add("invisible")
+   }
+ })
+ //addItem() if fields are filled in. return false if not.
+ return pass
 }
-
+ 
 function formUpdateTask(){
-  //Stage website for updating a task
-  updating = true;
-  document.querySelector('#taskTitle').textContent = "Update Task"
-  addTaskButton.textContent = "Save"
+ updating = true;
+ document.querySelector('#taskTitle').textContent = "Update Task"
+ addTaskButton.textContent = "Save"
 }
 function formNewTask(){
-  //Stage website for creating a new task
-  taskUpdate = "";
-  updating = false;
-  document.querySelector('#taskTitle').textContent = "New Task"
-  addTaskButton.textContent = "Add Task"
+ taskUpdate = "";
+ updating = false;
+ document.querySelector('#taskTitle').textContent = "New Task"
+ addTaskButton.textContent = "Add Task"
 }
 function clearForms(){
-  //Clear all inputs
-  userInput.forEach((i)=>{ i.value = "" })
-  showCards()
+ userInput.forEach((i)=>{ i.value = "" })
+ showCards()
 }
 function fillForm(data){
-  //data = {}
-  //Populate all inputs with appropiate values from {data}
-  userInput.forEach(function(i){
-    let key = i.getAttribute("key")
-    i.value = data[key]
-  })
+ userInput.forEach(function(i){
+   let key = i.getAttribute("key")
+   i.value = data[key]
+ })
 }
 function hideSiblings(task){
-  //Hides siblings and pops out the current task
-  task.pop();
-  //Loop through taskList and call .hide(), excluding the given task
-  for(let i in taskList)
-    if(taskList[i].info.id!==task.info.id) taskList[i].hide()
+ task.pop();
+ for(let i in taskList)
+   if(taskList[i].info.id!==task.info.id) taskList[i].hide()
 }
 function showCards(){
-  //Resets all cards with .resetStyle
-  for(let i in taskList) taskList[i].resetStyle()
+ for(let i in taskList) taskList[i].resetStyle()
 }
 function getClickedTaskItem(e){
-  //e = element
-  //Returns a single task from taskList
-  let id = parseInt(e.target.closest('.card').getAttribute("id").replace('task_',''));
-  return taskList[id]
+ let id = parseInt(e.target.closest('.card').getAttribute("id").replace('task_',''));
+ return taskList[id]
 }
-
 /**On Click Functions */
 function onCancelClick(){
-  clearForms()
-  if(updating) formNewTask()
+ clearForms()
+ if(updating) formNewTask()
 }
 function onAddTaskClick(){
-  //Invokes validateInput() inside an if function
-  if(validateInput()){
-    //If not updating, create a new card. otherwise, update the card. 
-    if(!updating) addItem()
-    else{
-      taskUpdate.updateInfo(getObjFromForm())
-      formNewTask()
-    }
-    clearForms()
-  }
+ //Calling validateInput() inside an if function
+ if(validateInput()){
+   if(!updating) addItem()
+   else{
+     taskUpdate.updateInfo(getObjFromForm())
+     formNewTask()
+   }
+   clearForms()
+   save()
+ }
 }
 function onTaskContainerClick(e){
-  if(e.target.classList.contains('btn-success')){
-    let quote = getClickedTaskItem(e)
-    let change = quote.info
-    change.status = 'Completed'
-    quote.updateInfo(change)
-  }else{
-    if(updating) showCards();
-
-    let selectedTask = getClickedTaskItem(e)
-    taskUpdate = selectedTask;
-
-    let clickedTaskData = selectedTask.info
-    fillForm(clickedTaskData)
-    formUpdateTask()
-
-    hideSiblings(selectedTask)
-    document.documentElement.scrollTop = 0;
-  }
+ if(e.target.classList.contains('btn-success')){
+let quote = getClickedTaskItem(e)
+let change = quote.info
+change.status = 'Completed'
+quote.updateInfo(change)
+save()
+ }else{
+ if(updating) showCards();
+ 
+ let selectedTask = getClickedTaskItem(e)
+ taskUpdate = selectedTask;
+ 
+ let clickedTaskData = selectedTask.info
+ fillForm(clickedTaskData)
+ formUpdateTask()
+ 
+ hideSiblings(selectedTask)
+ document.documentElement.scrollTop = 0;
+ }
 }
+function save() {
+ for(let i in taskList) {
+   let value = JSON.stringify(taskList[i].info)
+   let key = taskList[i].info.id
+console.log(value)
+   localStorage.setItem(key, value)
+ }
+}
+function load() {
+ console.log(localStorage.getItem('task_0'))
+ let counter = 0
+ let currentTask = JSON.parse(localStorage.getItem('task_'+counter))
+ while(currentTask) {
+   console.log(currentTask)
+   currentTask.id = ''
+   addItem(currentTask)
+   counter++
+ 
+   currentTask = JSON.parse(localStorage.getItem('task_'+counter))
+ }
+ 
+ }
